967011fed2fa78d17516d284c81f523d2deec26c
Eninix <enin.ix@qq.com>
2022-03-04 22:51:09 +0800

    ver_2022.3.4

--- book/script/demo16_http/http_utils.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-const HttpUtils = preload("res://zfoo/util/HttpUtils.gd")
-
-func _ready():
-	var util = HttpUtils.new()
-	var response = util.get("https://www.zhihu.com")
-	print(response)
--- build-for-push.sh
+++ build-for-push.sh
@@ -2,7 +2,16 @@
 
 mdbook build
 echo "= = = = = build complete = = = = ="
+
 rm -rf ./book/.git
 rm ./book/.gitignore
-echo "= = = = = .git have been deleted = = = = ="
+echo "= = = = = .git file that have been deleted = = = = ="
+
 cp SUMMARY.md SUMMARY.md.bak
+
+rm -rf ./book/zfoo
+rm -rf ./book/script
+rm -rf ./book/scene
+rm -rf ./book/gdTest
+rm -rf ./book/gdProtocol
+echo "= = = = = Nonregard file that have been deleted = = = = ="
